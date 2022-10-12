d7580b2df873a4008a7aa59847090abbac87cf63
ChrisKellFSD <chriskellcsn@gmail.com>
2022-10-12 15:01:43 +0000

    Initial Commit

---
+++ assets/js/myscript.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("h2").css("text-decoration", "underling"); //underlines all <h2> elements
+    $("ul").css("border", "solid 1px #ccc"); //adds a border to all the lists
+});
